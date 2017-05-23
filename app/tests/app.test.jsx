8a31ce4c78201979cecc38d0e264f04e9ef8c394
nazimkazim <nazik_111@mail.ru>
2017-05-23 18:37:42 +0600

    Add clock component, tests, style

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -1,10 +1,6 @@
 var expect = require('expect');
 
 
-it('should properly run tests', ()=> {
-    expect(1).toBe(1);
-});
-
 describe('App', ()=> {
     it('should properly run tests', ()=> {
         expect(1).toBe(1);
