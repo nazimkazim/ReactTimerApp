d40e75fc395f56ca993400fba6f867516cec132f
nazimkazim <nazik_111@mail.ru>
2017-05-27 16:47:55 +0600

    Add countdown functionality

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config) {
     config.set({
-        borwsers:['Chrome'],
+        browsers:['Chrome'],
         singleRun:true,
         frameworks:['mocha'],
         files:['app/tests/**/*.test.jsx'],
